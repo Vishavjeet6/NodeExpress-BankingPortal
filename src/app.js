35b4aa45beaaa2618d4df217b3b2abce84aafc69
Vishavjeet6 <vishavjeetsinghthakur@gmail.com>
2020-08-11 12:07:21 +0530

    first

--- src/app.js
+++ src/app.js
@@ -10,9 +10,24 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json',{encoding: 'UTF8'});
+const account = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json',{encoding: 'UTF8'});
+const user = JSON.parse(userData);
+
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'});
+    res.render('index', {title: 'InAccount Summarydex',accounts: accounts});
+});
+app.get('/savings', (req, res) => {
+    res.render('account', {account: accounts.savings});
+});
+app.get('/checking', (req, res) => {
+    res.render('account', {account: accounts.checking});
+});
+app.get('/credit', (req, res) => {
+    res.render('account', {account: accounts.credit});
 });
 
 
