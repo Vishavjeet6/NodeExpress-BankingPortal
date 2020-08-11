1c2bc9addc8e5064e26c1cc4c5b87ba4b6c6869a
Vishavjeet6 <vishavjeetsinghthakur@gmail.com>
2020-08-11 15:10:16 +0530

    first

--- src/app.js
+++ src/app.js
@@ -4,19 +4,20 @@ const express = require('express');
 const app = express();
 const { accounts, users, writeJSON } = require('./data');
 const accountRoutes = require('./routes/accounts')
-const serviceRoutes = require('./routes/services')
+const servicesRoutes = require('./routes/services')
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use('/account', accountRoutes);
-app.use('/services', serviceRoutes);
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Account Summary',accounts: accounts});
 });
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 
 app.get('/profile', (req, res) =>  {
